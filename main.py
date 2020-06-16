3fd57877d8aba71fb75e011e499328c19d43e167
Zhanibek <janibek060639@gmail.com>
2020-06-16 14:20:09 +0600

    login and register seems to be working

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from models import *
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://abfqprtnmjmzrt:336db6044bedcf620c4ce47070642e937396b7c5633d24d07b759103d3bbd9ce@ec2-54-247-79-178.eu-west-1.compute.amazonaws.com:5432/d6tia03nnbe25e" #os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://dgpohzutvnfioc:b66825f5296b8648d10b630f565dd0bc9d31b99ec0250686b743b59460d80157@ec2-54-217-224-85.eu-west-1.compute.amazonaws.com:5432/d7kd8qe1q0i3h1" #os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
@@ -21,18 +21,18 @@ def menu():
 	password = request.form.get("password")
 
 	option = request.form.get("option")
-	if option=="login"
-		check = Flight.query.filter_by(name=username, password=password).first()
+	if option=="login":
+		check = User.query.filter_by(name=username, password=password).first()
 		if check:
 			return render_template("search.html")
 		return render_template("error.html", message="Username or password is incorrect")
 
 	elif option=="register":
-		exists = Flight.query.filter_by(name=username).first()
+		exists = User.query.filter_by(name=username).first()
 		if exists:
 			print("This username already exists")
 			return render_template("error.html", message="Username already exists")
-		User.add_user(username, password)
+		User.add_user(name=username, passwrd=password)
 		return render_template("index.html")
 
 
--- models.py
+++ models.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-class Book(db.model):
+class Book(db.Model):
     __tablename__ = "books"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
@@ -21,7 +21,7 @@ class User(db.Model):
     name = db.Column(db.String, nullable=False)
     password = db.Column(db.String, nullable=False)
 
-    def add_user(self, name, passwrd):
+    def add_user(name, passwrd):
     	u = User(name=name, password=passwrd)
     	db.session.add(u)
     	db.session.commit()
