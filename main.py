3f861ff573005dd04bd0b83d8c286e78b3291cb3
Zhanibek <janibek060639@gmail.com>
2020-06-17 00:37:01 +0600

    Sessions added. Now I know who is logged in rn. Time to add reviews

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, session
 from sqlalchemy import or_
 from models import *
 
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://dgpohzutvnfioc:b66825f5296b8648d10b630f565dd0bc9d31b99ec0250686b743b59460d80157@ec2-54-217-224-85.eu-west-1.compute.amazonaws.com:5432/d7kd8qe1q0i3h1" #os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
-
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 @app.route("/")
 def index():
@@ -24,13 +24,13 @@ def menu():
 	if option=="login":
 		check = User.query.filter_by(name=username, password=password).first()
 		if check:
+			session['user'] = username
 			return render_template("search.html")
 		return render_template("error.html", message="Username or password is incorrect")
 
 	elif option=="register":
 		exists = User.query.filter_by(name=username).first()
 		if exists:
-			print("This username already exists")
 			return render_template("error.html", message="Username already exists")
 		User.add_user(name=username, passwrd=password)
 		return render_template("index.html")
